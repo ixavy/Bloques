d143528476f6be284298de8bfe8ae9ad3e3bd588
javier garcia <javier@idraw.es>
2013-01-25 15:44:09 +0100

    Initial Commit

---
+++ Bloques/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Bloques
+//
+//  Created by Javier García Gallego on 25/01/13.
+//  Copyright (c) 2013 ixavy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
